acac57cf5273f25694f58103d40493844166cb5f
Mercy <maritimmercy8@gmail.com>
2025-06-22 22:01:38 +0300

    added models/episode & apperance

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+from server.controllers.auth_controller import auth_bp
 
 from server.extensions import db, migrate
 from server.config import SQLALCHEMY_DATABASE_URI
@@ -12,4 +13,5 @@ def create_app():
     migrate.init_app(app, db)
     CORS(app)
 
+    app.register_blueprint(auth_bp)
     return app
---
+++ server/models/appearance.py
@@ -0,0 +1,12 @@
+from server.extensions import db
+
+class Appearance(db.Model):
+    __tablename__ = 'appearances'
+
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+
+    guest_id = db.Column(db.Integer, db.ForeignKey('guests.id'), nullable=False)
+    episode_id = db.Column(db.Integer, db.ForeignKey('episodes.id'), nullable=False)
+
+    
